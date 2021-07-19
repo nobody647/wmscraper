dccfb2dd887c924cf415cd8702c545b362446284
= <=>
2021-07-18 21:35:58 -0400

    bit of cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ use scraper::Html;
 use scraper::Selector;
 use std::fmt;
 
+enum Size {
+    HalfGram,
+    OneGram,
+}
+
 struct Listing {
     name: String,
     brand: Option<String>,
@@ -29,17 +34,16 @@ fn main() {
 fn scrape_page(url: &str) -> Result<Vec<Listing>, reqwest::Error> {
     //download the entire page
     let body = reqwest::blocking::get(url)?.text()?;
-    println!("{}",body);
+    //println!("{}",body);
 
     //scrape for product entries
     let body = Html::parse_document(&body);
     let selector = Selector::parse("[data-test-id=menu-item-list-item]").unwrap();
 
     //extract data
-    let mut list = Vec::new();
-    for element in body.select(&selector) {
-        println!("Element with id {:?} found", element.value().attr("data-test-id"));
-        list.push(scrape_entry(&element));
+    let mut list = Vec::new(); //vector of product listings
+    for element in body.select(&selector) { //for each listing element
+        list.push(scrape_entry(&element)); //scrape and add to list
     }
 
     Ok(list)
@@ -57,6 +61,6 @@ fn scrape_entry(element: &scraper::ElementRef) -> Listing {
             Some(elm) => Some(elm.inner_html()),
             None => None,
         }, //this should be one line
-        price: element.select(&price_selector).find(|&elm| elm.inner_html().starts_with('$')).unwrap().inner_html(),
+        price: element.select(&price_selector).find(|&elm| elm.inner_html().starts_with('$') ).unwrap().inner_html(), //yeahhhhh
     }
 }
